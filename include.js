6030d185c526064ed3d8064860e28ebb8ac09504
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-07-12 20:25:10 +0530

    add art from kaustubh

--- include.js
+++ include.js
@@ -6506,7 +6506,7 @@ let cards = [
     author: "mbera99",
     githubLink: "https://github.com/mbera99",
   },
-      {
+  {
     artName: 'Ducky',
     pageLink: './Art/cpk3/index.html',
     imageLink: './Art/cpk3/Ducky.gif',
@@ -6526,6 +6526,14 @@ let cards = [
     author: 'Marion',
     githubLink: 'https://github.com/marionjudy13'
   },
+  {
+    artName: 'Red Red Square',
+    pageLink: './Art/kaustubh43/index.html',
+    imageLink: './Art/kaustubh43/pic.gif',
+    author: 'kaustubh43',
+    githubLink: 'https://github.com/kaustubh43'
+  }
+
 ];
 
 // +--------------------------------------------------------------------------------+
@@ -6540,14 +6548,14 @@ let contents = [];
 Shuffle(cards).forEach((c) => {
   contents.push([
     `<li class="card">` +
-      `<a href='${c.pageLink}'>` +
-      `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
-      `</a>` +
-      `<div class="flex-content">` +
-      `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
-      `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
-      `</div>` +
-      `</li>`,
+    `<a href='${c.pageLink}'>` +
+    `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
+    `</a>` +
+    `<div class="flex-content">` +
+    `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
+    `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
+    `</div>` +
+    `</li>`,
   ]);
 });
 
